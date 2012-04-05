efafcbdbaf6220ce9ff0174b9a69e2443ab99e79
Sever Băneşiu <banesiu.sever@gmail.com>
2012-04-05 10:25:05 +0300

    Bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
 
     name='0x10c-asm',
-    version='0.0.1',
+    version='0.0.2',
     description="A simple Python-based DCPU assembly compiler",
     long_description=open('README.rst').read(),
     keywords='notch asm dcpu-16 dcpu assembly asm',

ad9553d093e90ecb17a4cc21a4941cf8ccee008a
Sever Băneşiu <banesiu.sever@gmail.com>
2012-04-05 09:09:59 +0300

    Added setup and readme.

---
+++ README.rst
@@ -0,0 +1,5 @@
+``0x10c-asm`` assembly compiler for Notch's DCPU-16
+---------------------------------------------------
+
+To install from Pypi:
+  pip install 0x10c-asm
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+
+    name='0x10-asm',
+    version='0.0.1',
+    description="A simple Python-based DCPU assembly compiler",
+    long_description=open('README.rst').read(),
+    keywords='notch asm dcpu-16 dcpu assembly asm',
+    author='Sever Banesiu',
+    author_email='banesiu.sever@gmail.com',
+    url='https://github.com/severb/graypy',
+    license='BSD License',
+    scripts=['0x10-asm.py'],
+    zip_safe=False,
+
+)
